44a11b7e20bd5dc72b17b2a915ad9db209c9a6a6
IsraelGouveia <israel.souza@mobile2you.com.br>
2022-04-02 18:10:02 -0300

    psql database configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,14 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
+  # config/environments/production.rb
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
